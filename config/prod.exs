5aa0f448ccaf2f770fa41905cdca7342622c1034
Wojtek Mach <wojtek@wojtekmach.pl>
2020-11-26 07:34:08 +0100

    Update

--- config/prod.exs
+++ config/prod.exs
@@ -1,7 +1,7 @@
 import Config
 
 config :phoenix_example, PhoenixExampleWeb.Endpoint,
-  url: [host: "tranquil-shelf-54417.herokuapp.com", port: 443],
+  # url: [host: "tranquil-shelf-54417.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
